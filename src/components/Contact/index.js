b808f22d701d791f4c620da89812d2c31310eaca
Sinaswrld <s2vahidi@uwaterloo.ca>
2023-02-11 12:42:38 -0500

    Portfolio up to now

---
+++ src/components/Contact/index.js
@@ -0,0 +1,117 @@
+import './index.scss'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_rfdl8de',
+        'template_0l3jtah',
+        refForm.current,
+        '28SzXRU9fZyp1CQx0'
+      )
+      .then(
+        (result) => console.log(result.text),
+        (error) => console.log(error.text)
+      )
+  }
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={'Contact me'.split('')}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in co-op opportunities - especially ambitious or
+            large projects. However, if you have any other requests or
+            questions, don't hesitate to contact me.
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input
+                    type="text"
+                    name="from_name"
+                    placeholder="Name"
+                    required
+                  />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="from_email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="from_subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Sina Vahidi,
+          <br />
+          Canada,
+          <br />
+          Newmarket, ON
+          <br />
+          <span>s2vahidi@uwaterloo.ca</span>
+          <br />
+          <span>647-671-3324</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[44.0618657, -79.5010341]} zoom={5}>
+            <TileLayer
+              url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
+            />
+            <Marker position={[44.0618657, -79.5010341]}>
+              <Popup>I live around here, come over for a cup of coffee!</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,125 @@
+import './index.scss'
+import { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faClose,
+  faBars,
+  faEnvelope,
+  faHome,
+  faUser,
+  faSuitcase,
+} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <div className="nav-items-container">
+          <NavLink
+            onClick={() => setShowNav(false)}
+            exact="true"
+            activeclassname="active"
+            className="home-link"
+            to="/"
+            end
+          >
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            onClick={() => setShowNav(false)}
+            exact="true"
+            activeclassname="active"
+            className="about-link"
+            to="/about"
+          >
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            onClick={() => setShowNav(false)}
+            exact="true"
+            activeclassname="active"
+            className="portfolio-link"
+            to="/portfolio"
+          >
+            <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            onClick={() => setShowNav(false)}
+            exact="true"
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+          <FontAwesomeIcon
+            icon={faClose}
+            color="#ffd700"
+            size="3x"
+            className="close-icon"
+            onClick={() => setShowNav(false)}
+          />
+        </div>
+        <ul className="mobile">
+          <li>
+            <a
+              className="linkedin-link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/sina-vahidi"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="github-link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/Sinaswrld"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/sina-vahidi"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Sinaswrld"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+        onClick={() => setShowNav(true)}
+      />
+    </div>
+  )
+}
+
+export default Sidebar
